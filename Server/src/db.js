0a29be18d475b12ad0378b6741488285e9ee4ee5
Alejo Funes Abdala <alejofunes1@gmail.com>
2023-12-29 12:39:11 -0300

    deploy_back

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const {  DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 const ModelVideogame = require('./models/Videogame');
 const ModelGenre = require('./models/Genre')
 const ModelPlatform = require('./models/Platform')
 
 
-const sequelize = new Sequelize(
+/* const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  { logging: false, native: false }
+); */
+
+const sequelize = new Sequelize(DB_DEPLOY,
   { logging: false, native: false }
 );
 
